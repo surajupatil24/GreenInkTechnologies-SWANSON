78c993eea1b9b9d4fb26c58fe72771e4d7fae140
Suraj Patil <66869244+surajupatil24@users.noreply.github.com>
2022-03-21 13:37:50 +0530

    pallettype
    pallettype

--- routes/index.js
+++ routes/index.js
@@ -83,12 +83,12 @@ router.post('/generatepdf', function (req, res, next) {
 		const dataFormated = filtered[0];
 
 		const row1Text = `(91) ${dataFormated.IRMSGCAS}(37)${quantity}`;
-		const row2Text = `(10)AHNL${batch}~(90)NONE`;
-		const row3Text = `(00) 1 1534145 40011${quantity6} 9`;
+		const row2Text = `(10)AHNL${batch}~(90)${quantity6}`;
+		const row3Text = `(00) 1 1534145 40011${quantity4} 9`;
 
 		GenerateBarCodeForNumber(`91${dataFormated.IRMSGCAS}37${quantity}`).then(image1=>{
-			GenerateBarCodeForNumber(`10AHNL${batch}90`).then(image2=>{
-				GenerateBarCodeForNumber(`001153414540011${quantity6}9`).then(image3=>{
+			GenerateBarCodeForNumber(`10AHNL${batch}90${quantity6}`).then(image2=>{
+				GenerateBarCodeForNumber(`001153414540011${quantity4}9`).then(image3=>{
 					
 					dataFormated.lotNumber = batch;
 					dataFormated.prdDate = prdDate;
@@ -270,7 +270,7 @@ function formatPDFWithData(data) {
 								{ text:`BASIS WEIGHT:    	   ${data.PO} `, style: 'subheader' },
 							],
 							[
-								{ text: `PALLETS CONTANINTER : ${data.quantity6}`, style: 'subheader' },
+								{ text: `PALLET TYPE : ${data.quantity6}`, style: 'subheader' },
 							]
 						],
 						[
